6bef0dea24fb1c4e2c08c0efe4bfdb6b339b613a
Ivan Tytarenko <ivant@wix.com>
2021-01-26 11:46:38 +0200

    adds test

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  printWidth: 100,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'es5',
   bracketSpacing: true,
-  jsxBracketSameLine: false,
   tabWidth: 2,
   semi: true,
 };
