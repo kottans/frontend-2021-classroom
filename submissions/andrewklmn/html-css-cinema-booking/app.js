3347848c8836177476cd95de16c317cbca84aab5
Andrew <Klmn>
2021-02-03 11:05:15 +0200

    Add: aria-tags for a11y

--- submissions/andrewklmn/html-css-cinema-booking/app.js
+++ submissions/andrewklmn/html-css-cinema-booking/app.js
@@ -8,35 +8,38 @@ const seatInitialSet = [
   [FREE, BOOKED, BOOKED, BOOKED, FREE, FREE, FREE, FREE, FREE],
   [FREE, FREE, FREE, FREE, FREE, FREE, FREE, FREE, FREE],
   [FREE, BOOKED, BOOKED, BOOKED, FREE, FREE, FREE, FREE, FREE],
-  [FREE, BOOKED, BOOKED, BOOKED, FREE, FREE, FREE, FREE, FREE],
   [FREE, FREE, BOOKED, BOOKED, BOOKED, BOOKED, BOOKED, FREE, FREE, FREE, FREE],
 ];
 
 const init = (seats) => {
   seats.forEach((row, rowIndex) => {
     const seatRow = document.createElement('section');
+    const r = rowIndex + 1;
     seatRow.classList.add('seat_row');
+
     row.forEach((seat, seatIndex) => {
       const seatElement = document.createElement('article');
-      const r = rowIndex + 1;
       const s = seatIndex + 1;
       const d = seat === BOOKED ? 'disabled' : '';
 
       seatElement.classList.add('seat');
+      seatElement.setAttribute('role', 'none');
       seatElement.innerHTML = `
         <input 
-          class="seat_control" 
-          type="checkbox" 
-          id="seat_${r}_${s}" 
-          name="seat_${r}_${s}" 
-          value="booked" ${d}>
-        <label class="seat_label" for="seat_${r}_${s}">${s}</label>
+            class="seat_control" 
+            type="checkbox"
+            id="seat_${r}_${s}" 
+            name="seat_${r}_${s}" 
+            aria-label="row${r}, seat${s}"
+            value="booked" ${d}>
+        <label class="seat_label" aria-hidden="true" for="seat_${r}_${s}">${s}</label>
       `;
       seatRow.appendChild(seatElement);
     });
     seatContainer.appendChild(seatRow);
   });
-  seatContainer.innerHTML += '<button type="submit">Book now</button>';
+
+  /* seatContainer.innerHTML += '<button class="book_button" type="submit">Buy tickets</button>'; */
 };
 
 document.addEventListener('DOMContentLoaded', () => {
