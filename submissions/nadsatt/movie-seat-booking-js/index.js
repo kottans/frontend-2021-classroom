82a86fe579b0d10f7f9e9b94ae40024dcc78b990
nadsatt <yabybka@gmail.com>
2021-02-25 22:27:20 +0200

    refactor code

--- submissions/nadsatt/movie-seat-booking-js/index.js
+++ submissions/nadsatt/movie-seat-booking-js/index.js
@@ -1,13 +1,13 @@
 (() => {
-  const FORM = document.querySelector('.movie-form');
-  const FORM_INPUTS = FORM.querySelectorAll('input:not([name="direction"])');
-  const FORM_SEAT_CONTROL = document.querySelector('.seat-control');
+  const FORM_INPUTS = document.querySelectorAll(
+    'input:not([name="direction"])'
+  );
+  const SEAT_CONTROL = document.querySelector('.seat-control');
   const SUBMIT_BUTTON = document.querySelector('.submit-button');
   const TICKET_WRAPPER = document.querySelector('.ticket-wrapper');
   const TICKET_VALUES = document.querySelectorAll('.ticket__value');
   const ORDER_INFO_LIST = document.querySelector('.order__info-list');
-  const ORDER_PLACEHOLDER_ITEM = document.querySelector('.order__placeholder-item');
-  const ORDER_TOTAL_PRICE_VALUE = document.querySelector('.order__total-price-value');
+  const ORDER_TOTAL_PRICE = document.querySelector('.order__total-price-value');
   const SEAT_PRICE = 18;
   const RENDERED_ORDER_INFO_ITEMS = {};
 
@@ -22,7 +22,7 @@
     hideTicketWrapper();
     resetTicket();
   });
-  FORM_SEAT_CONTROL.addEventListener('input', updateOrderInfoList);
+  SEAT_CONTROL.addEventListener('input', updateOrderInfoList);
 
   function fillTicket(inputs) {
     const checkedInputs = getCheckedInputs(inputs);
@@ -43,17 +43,19 @@
   function getValues(inputs) {
     const values = {};
 
-    values.day = inputs.find(input => input.name === 'day').value;
-    values.time = inputs.find(input => input.name === 'time').value;
-    values.hall = inputs.find(input => input.name === 'time').dataset.hall;
-    values.seat = inputs.filter(input => input.name === 'seat')
-      .map(({value}) => {
+    values.day = inputs.find((input) => input.name === 'day').value;
+    values.time = inputs.find((input) => input.name === 'time').value;
+    values.hall = inputs.find((input) => input.name === 'time').dataset.hall;
+    values.seat = inputs
+      .filter((input) => input.name === 'seat')
+      .map(({ value }) => {
         const [row, seat] = value.split('_');
         return `<li class="ticket__seat-value-item">Row
                   <span class="ticket__row-value">${row}</span>/ Seat
                   <span class="ticket__seat-value">${seat}<span>
-                </li>`
-      }).join('');
+                </li>`;
+      })
+      .join('');
 
     return values;
   }
@@ -79,14 +81,10 @@
   }
 
   function addOrderInfoItem(value) {
-    if (isOrderInfoListEmpty()) {
-      ORDER_PLACEHOLDER_ITEM.remove();
-    }
-
     const infoItem = createOrderInfoItem(value);
     ORDER_INFO_LIST.append(infoItem);
     RENDERED_ORDER_INFO_ITEMS[value] = infoItem;
-    ORDER_TOTAL_PRICE_VALUE.textContent = +ORDER_TOTAL_PRICE_VALUE.textContent + SEAT_PRICE;
+    ORDER_TOTAL_PRICE.textContent = +ORDER_TOTAL_PRICE.textContent + SEAT_PRICE;
   }
 
   function createOrderInfoItem(value) {
@@ -105,17 +103,9 @@
     return infoItem;
   }
 
-  function isOrderInfoListEmpty() {
-    return Object.keys(RENDERED_ORDER_INFO_ITEMS).length === 0;
-  }
-
   function removeOrderInfoItem(value) {
     RENDERED_ORDER_INFO_ITEMS[value].remove();
     delete RENDERED_ORDER_INFO_ITEMS[value];
-    ORDER_TOTAL_PRICE_VALUE.textContent = +ORDER_TOTAL_PRICE_VALUE.textContent - SEAT_PRICE;
-
-    if (isOrderInfoListEmpty()) {
-      ORDER_INFO_LIST.append(ORDER_PLACEHOLDER_ITEM);
-    }
+    ORDER_TOTAL_PRICE.textContent = +ORDER_TOTAL_PRICE.textContent - SEAT_PRICE;
   }
 })();
