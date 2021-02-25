8f7bb331acee099b44724acd9dd81d0c4a4777c3
Andrii Klymeniuk <andrew.klmn@gmail.com>
2021-02-25 19:14:30 +0200

    Delete screen-switcher.js

--- submissions/andrewklmn/js-movie-seat-booking/modules/screen-switcher.js
+++
@@ -1,5 +0,0 @@
-export default function switchScreens(screenToShow, screenToHide, button) {
-  screenToHide.classList.add('hidden');
-  screenToShow.classList.remove('hidden');
-  button.focus();
-}
