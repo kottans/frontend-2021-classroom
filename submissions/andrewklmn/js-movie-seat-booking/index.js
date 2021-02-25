ddc72838503073b6b9fd3464cbb3996a310e9a0b
Andrii Klymeniuk <andrew.klmn@gmail.com>
2021-02-25 19:13:36 +0200

    Update: index.js

--- submissions/andrewklmn/js-movie-seat-booking/index.js
+++ submissions/andrewklmn/js-movie-seat-booking/index.js
@@ -1,19 +1,18 @@
 import {
-  mainScreen,
+  screens,
   selectorForm,
   bookButton,
-  confirmationScreen,
   cancelButton,
   payButton,
 } from './module/config';
 
-import switchScreens from './module/screen-switcher';
+import showScreen from './module/screen-shower';
 import { payButtonListener, bookButtonListener } from './module/button-listeners';
 import updateTicket from './module/ticket-updater';
 
 document.addEventListener('DOMContentLoaded', () => {
   selectorForm.addEventListener('change', updateTicket);
   bookButton.addEventListener('click', bookButtonListener);
-  cancelButton.addEventListener('click', () => switchScreens(mainScreen, confirmationScreen, bookButton));
+  cancelButton.addEventListener('click', () => showScreen(screens.main, bookButton));
   payButton.addEventListener('click', payButtonListener);
 });
