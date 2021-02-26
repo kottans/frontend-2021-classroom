78fc52da49755466ddb7351f64faf4878661bff6
Ihor <vedunigor@gmail.com>
2021-02-26 14:27:01 +0200

    Added task movie-seat-booking-js

---
+++ submissions/ik-web/movie-seat-booking-js/js/script.js
@@ -0,0 +1,161 @@
+import {seatsData} from './seats.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const startApp = () => {
+        const cinemaHall = document.querySelector('#seats-form');
+
+        addSeatsToPage(seatsData, cinemaHall);
+        doBookingSeats(cinemaHall);
+    };
+
+    const addSeatsToPage = (seatsData, cinemaHall) => {
+
+        seatsData.forEach(rowData => {
+            const row = createRow();
+
+            rowData.forEach(seatData => {
+                row.innerHTML += createSeat(seatData);
+            });
+
+            cinemaHall.append(row);
+        })
+    }
+
+    const createRow = () => {
+        const row = document.createElement('ol');
+        row.classList.add('cinema-hall__row');
+        return row;
+    }
+
+    const createSeat = ({seatId, row, seat, price, booked}) => {
+        return `
+            <li>
+                <label>
+                    <input type="checkbox" class="cinema-hall__check" data-row="${row}" data-seat="${seat}" value="${seatId}" data-price="${price}" ${booked ? "disabled" : ""}>
+                    <span class="cinema-hall__seat">${seat}</span>
+                </label>
+            </li>
+        `;
+    }
+
+    const doBookingSeats = cinemaHall => {
+        const selectedSeatsTotal = document.querySelector('.cinema-hall__seats-total-info');
+        const modalInfo = document.querySelector('.modal__info');
+        let selectedSeatsCount = 0;
+        let selectedSeatsCost = 0;
+        
+        cinemaHall.addEventListener('change', ({target}) => {
+            
+            const seat = target.closest('input');
+            const seatPrice = +seat.getAttribute('data-price');
+            
+            if (!seat) return;
+            if (seat.checked) {
+                showSelectedSeat(seat);
+                selectedSeatsCount++
+                selectedSeatsCost += seatPrice;
+            } else {
+                removeSelectedSeat(seat);
+                selectedSeatsCount--
+                selectedSeatsCost -= seatPrice;
+            };
+            
+            let totalSelectedSeatsInfo = getTotalSelectedSeatsInfo(selectedSeatsCount, selectedSeatsCost);
+            showTotalSelectedSeatsInfo(selectedSeatsTotal, totalSelectedSeatsInfo);
+            handleSelectedSeatsBlock(selectedSeatsCount);
+            if (doClickButtonContinue) showTotalSelectedSeatsInfo(modalInfo, totalSelectedSeatsInfo);
+        });
+        
+        doClickButtons();
+
+    }
+
+    const doClickButtons = () => {
+        const modalWindow = document.querySelector('.modal');
+        doClickButtonContinue(modalWindow);
+        doClickButtonReturn(modalWindow);
+    };
+
+    const doClickButtonContinue = modalWindow => {
+        const buttonContinue = document.querySelector('.cinema-hall__button');
+
+        buttonContinue.addEventListener('click', () => {
+            modalWindow.classList.add('active');
+        });
+    }
+
+    const doClickButtonReturn = modalWindow => {
+        const buttonReturn = document.querySelector('.button--return');
+
+        buttonReturn.addEventListener('click', () => {
+            modalWindow.classList.remove('active');
+        });
+    }
+
+    const createSelectedSeat = ({selectedSeatId, rowNumber, seatNumber, selectedSeatPrice}) => {
+        return `
+            <div class="cinema-hall__selected-seat" data-id="${selectedSeatId}">
+                <div class="seat-info">
+                    Row: <span class="seat-info__row">${rowNumber}</span> --- Seat: <span class="seat-info__seat">${seatNumber}</span>
+                </div>
+                <div class="seat-price">
+                    ${selectedSeatPrice}
+                </div>
+            </div>
+        `;
+    };
+
+    const getSelectedSeatInfo = seat => {
+        const selectedSeatId = seat.value;
+        const rowNumber = seat.getAttribute('data-row');
+        const seatNumber = seat.getAttribute('data-seat');
+        const selectedSeatPrice = seat.getAttribute('data-price');
+        return {
+            selectedSeatId,
+            rowNumber,
+            seatNumber,
+            selectedSeatPrice
+        };
+    };
+
+    const showSelectedSeat = seat => {
+        const selectedSeatsList = document.querySelector('.cinema-hall__selected-seats-list');
+        selectedSeatsList.innerHTML += createSelectedSeat(getSelectedSeatInfo(seat));
+    };
+
+    const removeSelectedSeat = ({value}) => {
+        const selectedSeat = document.querySelector(`[data-id="${value}"]`);
+        selectedSeat.remove();
+    };
+
+    const handleSelectedSeatsBlock = selectedSeatsCount => {
+        const selectedSeatsBlock = document.querySelector('.cinema-hall__selected-seats-block');
+        const activeSelectedSeatsBlock = document.querySelector('.cinema-hall__selected-seats-block.active');
+        
+        if (selectedSeatsCount > 0 && !activeSelectedSeatsBlock) showSelectedSeatsBlock(selectedSeatsBlock);
+        else if (selectedSeatsCount === 0) hideSelectedSeatsBlock(selectedSeatsBlock);
+    };
+
+    const showTotalSelectedSeatsInfo = (pageElement, totalSelectedSeatsInfo) => {
+        pageElement.innerHTML = totalSelectedSeatsInfo;
+    };
+
+    const getTotalSelectedSeatsInfo = (selectedSeatsCount, selectedSeatsCost) => {
+        return `
+            <p>Total selected seats: ${selectedSeatsCount}</p>
+            <p>Total cost: $${selectedSeatsCost}</p>
+        `;
+    };
+
+    const showSelectedSeatsBlock = selectedSeatsBlock => {
+        selectedSeatsBlock.classList.add('active');
+    };
+
+    const hideSelectedSeatsBlock = selectedSeatsBlock => {
+        selectedSeatsBlock.classList.remove('active');
+    };
+
+    startApp();
+
+});
---
+++ submissions/ik-web/movie-seat-booking-js/js/seats.js
@@ -0,0 +1,264 @@
+const seatsData = [
+    [
+        {
+            seatId: 1,
+            row: 1,
+            seat: 1,
+            price: 4,
+            booked: true
+        },
+        {
+            seatId: 2,
+            row: 1,
+            seat: 2,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 3,
+            row: 1,
+            seat: 3,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 4,
+            row: 1,
+            seat: 4,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 5,
+            row: 1,
+            seat: 5,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 6,
+            row: 1,
+            seat: 6,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 7,
+            row: 1,
+            seat: 7,
+            price: 4,
+            booked: false
+        }
+    ],
+    [
+        {
+            seatId: 8,
+            row: 2,
+            seat: 1,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 9,
+            row: 1,
+            seat: 2,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 10,
+            row: 2,
+            seat: 3,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 11,
+            row: 2,
+            seat: 4,
+            price: 4,
+            booked: true
+        },
+        {
+            seatId: 12,
+            row: 2,
+            seat: 5,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 13,
+            row: 2,
+            seat: 6,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 14,
+            row: 2,
+            seat: 7,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 15,
+            row: 2,
+            seat: 8,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 16,
+            row: 2,
+            seat: 9,
+            price: 4,
+            booked: false
+        }
+    ],
+    [
+        {
+            seatId: 17,
+            row: 3,
+            seat: 1,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 18,
+            row: 3,
+            seat: 2,
+            price: 4,
+            booked: true
+        },
+        {
+            seatId: 19,
+            row: 3,
+            seat: 3,
+            price: 4,
+            booked: true
+        },
+        {
+            seatId: 20,
+            row: 3,
+            seat: 4,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 21,
+            row: 3,
+            seat: 5,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 22,
+            row: 3,
+            seat: 6,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 23,
+            row: 3,
+            seat: 7,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 24,
+            row: 3,
+            seat: 8,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 25,
+            row: 3,
+            seat: 9,
+            price: 4,
+            booked: false
+        }
+    ],
+    [
+        {
+            seatId: 26,
+            row: 4,
+            seat: 1,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 27,
+            row: 4,
+            seat: 2,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 27,
+            row: 4,
+            seat: 3,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 28,
+            row: 4,
+            seat: 4,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 29,
+            row: 4,
+            seat: 5,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 30,
+            row: 4,
+            seat: 6,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 31,
+            row: 4,
+            seat: 7,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 32,
+            row: 4,
+            seat: 8,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 33,
+            row: 4,
+            seat: 9,
+            price: 4,
+            booked: false
+        },
+        {
+            seatId: 34,
+            row: 4,
+            seat: 10,
+            price: 4,
+            booked: true
+        },
+        {
+            seatId: 35,
+            row: 4,
+            seat: 11,
+            price: 4,
+            booked: true
+        }
+    ]
+];
+
+export { seatsData };
