3a3abe4210478f55efd7e189a206bd294cb0234a
Andrii Klymeniuk <andrew.klmn@gmail.com>
2021-03-06 00:20:13 +0200

    Add: tab_disabled CSS rule for hidden screen

--- submissions/andrewklmn/js-movie-seat-booking/modules/screen-show.js
+++ submissions/andrewklmn/js-movie-seat-booking/modules/screen-show.js
@@ -4,9 +4,11 @@ export default function showScreen(activeScreen, focusedElement) {
   Object.keys(screens).forEach((key) => {
     if (screens[key] === activeScreen) {
       screens[key].classList.remove('hidden');
+      screens[key].classList.remove('tab_disabled');
       return;
     }
     screens[key].classList.add('hidden');
+    screens[key].classList.add('tab_disabled');
   });
   focusedElement.focus();
 }
