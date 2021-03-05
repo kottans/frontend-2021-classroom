804bb9b39b9398ccabf97cc3c1675134c50d3dd3
Sergey Gavrilov <ses3332002@gmail.com>
2021-03-05 19:52:20 +0200

    optimization

--- submissions/ses3332002/movie-seat-booking-js/script.js
+++ submissions/ses3332002/movie-seat-booking-js/script.js
@@ -22,38 +22,54 @@ const auditorium = document.querySelector('.auditorium');
 const preOrder = document.querySelector('.pre_order');
 const ticketsList = preOrder.querySelector('.pre_order__list');
 const orderConfirmationWindow = document.querySelector('.order_confirmation');
-const orderConfirmationText = orderConfirmationWindow.querySelector('.order_confirmation__text');
-const confirmButton = orderConfirmationWindow.querySelector('.order_confirmation__button-confirm');
-const cancelButton = orderConfirmationWindow.querySelector('.order_confirmation__button-cancel');
+const orderConfirmationText = orderConfirmationWindow.querySelector(
+  '.order_confirmation__text'
+);
+const confirmButton = orderConfirmationWindow.querySelector(
+  '.order_confirmation__button-confirm'
+);
+const cancelButton = orderConfirmationWindow.querySelector(
+  '.order_confirmation__button-cancel'
+);
 
 function prepareRow(rowData, rowIndex) {
   let rowString = '';
   rowData.forEach((item, i) => {
     switch (item) {
       case 0:
-        rowString += `<input type="checkbox" id="seat${`${rowIndex + 1}_${i + 1}`}"/><label for="seat${rowIndex + 1}_${
+        rowString += `<input type="checkbox" id="${`${rowIndex + 1}_${
+          i + 1
+        }`}"/><label for="${rowIndex + 1}_${
           i + 1
         }" class="seat" data-price="${regularSeatPrice}"></label>`;
         break;
       case 1:
-        rowString += `<input type="checkbox" id="seat${rowIndex + 1}_${i + 1}" disabled /><label for="seat${
-          rowIndex + 1
-        }_${i + 1}" class="seat"></label>`;
+        rowString += `<input type="checkbox" id="${rowIndex + 1}_${
+          i + 1
+        }" disabled /><label for="${rowIndex + 1}_${
+          i + 1
+        }" class="seat"></label>`;
         break;
       case 2:
-        rowString += `<input type="checkbox" id="seat${rowIndex + 1}_${i + 1}" disabled checked /><label for="seat${
-          rowIndex + 1
-        }_${i + 1}" class="seat"></label>`;
+        rowString += `<input type="checkbox" id="${rowIndex + 1}_${
+          i + 1
+        }" disabled checked /><label for="${rowIndex + 1}_${
+          i + 1
+        }" class="seat"></label>`;
         break;
       case 3:
-        rowString += `<input type="checkbox" id="seat${rowIndex + 1}_${i + 1}" data-type="vip"><label for="seat${
-          rowIndex + 1
-        }_${i + 1}" class="seat" data-price="${vipSeatPrice}" /></label>`;
+        rowString += `<input type="checkbox" id="${rowIndex + 1}_${
+          i + 1
+        }" data-type="vip"><label for="${rowIndex + 1}_${
+          i + 1
+        }" class="seat" data-price="${vipSeatPrice}" /></label>`;
         break;
       case 4:
-        rowString += `<input type="checkbox" id="seat${rowIndex + 1}_${
+        rowString += `<input type="checkbox" id="${rowIndex + 1}_${
           i + 1
-        }" disabled checked data-type="vip"/><label for="seat${rowIndex + 1}_${i + 1}" class="seat"/></label>`;
+        }" disabled checked data-type="vip"/><label for="${rowIndex + 1}_${
+          i + 1
+        }" class="seat"/></label>`;
     }
   });
   return rowString;
@@ -80,10 +96,12 @@ function prepareTicketsList() {
 }
 
 function calculatePreOrder() {
-  const checkedNodeList = seatsForm.querySelectorAll('input[type=checkbox]:checked:not(:disabled)');
+  const checkedNodeList = seatsForm.querySelectorAll(
+    'input[type=checkbox]:checked:not(:disabled)'
+  );
   if (checkedNodeList.length > 0) {
     selectedSeats = [...checkedNodeList].map(({ id, dataset }) => [
-      parseInt(id.slice(4), 10),
+      parseInt(id, 10),
       parseInt(id.slice(id.indexOf('_') + 1), 10),
       dataset.type,
     ]);
@@ -132,17 +150,22 @@ function confirmOrder() {
   function performTicketsString(ticketsNumber) {
     const words = ['билет', 'билета', 'билетов'];
     if (Math.abs(ticketsNumber % 100) > 20) ticketsNumber %= 10;
-    return words[(ticketsNumber > 4 || ticketsNumber === 0) + (ticketsNumber !== 1)];
+    return words[
+      (ticketsNumber > 4 || ticketsNumber === 0) + (ticketsNumber !== 1)
+    ];
   }
 
   function calculatePrice() {
     return selectedSeats.reduce(
-      (sum, item) => (item[2] === vipTag ? (sum += vipSeatPrice) : (sum += regularSeatPrice)),
+      (sum, item) =>
+        item[2] === vipTag ? (sum += vipSeatPrice) : (sum += regularSeatPrice),
       0
     );
   }
 
-  orderConfirmationText.innerHTML = `Вы заказали ${selectedSeats.length} ${performTicketsString(
+  orderConfirmationText.innerHTML = `Вы заказали ${
+    selectedSeats.length
+  } ${performTicketsString(
     selectedSeats.length
   )} на сумму ${calculatePrice()} грн`;
   orderConfirmationWindow.classList.toggle('order_confirmation-hidden');
