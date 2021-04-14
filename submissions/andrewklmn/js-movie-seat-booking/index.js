d5d3d2493743ee87dc43653934e9590a418a02d1
Andrii Klymeniuk <andrew.klmn@gmail.com>
2021-04-14 22:18:30 +0300

    Fix: reset order logic

--- submissions/andrewklmn/js-movie-seat-booking/index.js
+++ submissions/andrewklmn/js-movie-seat-booking/index.js
@@ -4,15 +4,22 @@ import {
   bookButton,
   cancelButton,
   payButton,
-} from './module/config';
+} from './modules/config';
 
-import showScreen from './module/screen-show';
-import { payButtonListener, bookButtonListener } from './module/button-listeners';
-import updateTicket from './module/ticket-updater';
+import showScreen from './modules/screen-show';
+
+import {
+  payButtonListener,
+  bookButtonListener,
+  cancelButtonListener,
+} from './modules/button-listeners';
+
+import updateTicket from './modules/ticket-updater';
 
 document.addEventListener('DOMContentLoaded', () => {
   selectorForm.addEventListener('change', updateTicket);
   bookButton.addEventListener('click', bookButtonListener);
+  cancelButton.addEventListener('click', cancelButtonListener);
   cancelButton.addEventListener('click', () => showScreen(screens.main, bookButton));
   payButton.addEventListener('click', payButtonListener);
 });
